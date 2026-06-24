4bb21d56e8d3f75793c248a6251af493bc0ed248
Nikhil <nikhilgupta542006@gmail.com>
2026-06-24 23:13:54 +0530

    create post functionality made

---
+++ src/services/post.service.ts
@@ -0,0 +1,9 @@
+   
+
+
+export default async function createPost(
+    clerkId:string
+){
+    
+}
+         
---
+++ src/validation/post.ts
@@ -0,0 +1,23 @@
+import {z} from "zod"
+
+export const postSchema=z.object({
+    content:z.string().trim()
+    .min(1,"more content is requitre")
+    .max(5000,"max 5000 content is valid "),
+
+      visibility:z.enum([
+         "COLLEGE_ONLY",
+          "PUBLIC"]
+      ),
+
+      images:z.array(z.string().url("Invalid image URL"))
+      .max(5,"max 5 images At a time is allowed ")
+       .default([]),
+
+
+      videos:z.array(z.string().url("Invalid url "))
+      .max(1,"max 1 video At a time is allowed ")
+       .default([]),
+
+    
+})
